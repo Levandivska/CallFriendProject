cf609e4d91ca93731e8028ed830c468363705e3d
olya <Levandivska@ucu.edu.ua>
2022-04-06 17:20:49 +0300

    Initial Commit

---
+++ CallFriendProject/CallFriendProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallFriendProjectApp.swift
+//  CallFriendProject
+//
+//  Created by оля on 06.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct CallFriendProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
